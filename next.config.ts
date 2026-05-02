327b9a26165a306f1d475f81df0fe0291199ff62
sumitkdevcode <kumar.sumit321321@gmail.com>
2026-05-02 10:38:38 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  compress: false, // Let cPanel handle compression to save memory
   /* config options here */
 };
 
