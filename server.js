87e22c65280b05834670d2ed13f7846a1db06e5c
sumitkdevcode <kumar.sumit321321@gmail.com>
2026-05-03 15:21:07 +0530

    update

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ process.env.HOSTNAME = '127.0.0.1';
 process.env.PORT = process.env.PORT || 3000;
 
 // AGGRESSIVE PROCESS/MEMORY LIMITS FOR SHARED HOSTING
+process.env.NEXT_TELEMETRY_DISABLED = '1';
 process.env.UV_THREADPOOL_SIZE = '1';
-process.env.NODE_OPTIONS = '--max-old-space-size=256';
+process.env.NODE_OPTIONS = '--max-old-space-size=256 --no-warnings';
 process.env.NEXT_MANUAL_SIG_HANDLE = 'true';
 
 console.log('Starting standalone Next.js server on port ' + process.env.PORT);
