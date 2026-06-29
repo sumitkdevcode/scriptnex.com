3ad98e7aa64915548ee931d92a5999f35d903451
sumitkdevcode <kumar.sumit321321@gmail.com>
2026-06-29 20:59:17 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -185,7 +185,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             }),
           }}
         />
-        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
